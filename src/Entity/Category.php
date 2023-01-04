99a68fe8193d3d9665b015ab610cf00b78270e79
Bmalik73 <bmalik01@live.fr>
2023-01-04 11:45:11 +0100

    update entity category

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -26,6 +26,11 @@ class Category
         $this->menus = new ArrayCollection();
     }
 
+    public function __toString(): string
+    {
+        return $this->getName();
+    }
+    
     public function getId(): ?int
     {
         return $this->id;
