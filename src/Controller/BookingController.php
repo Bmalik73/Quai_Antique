84b8383e517ad57f19ea33851de88d55cf786939
Bmalik73 <bmalik01@live.fr>
2023-01-06 11:21:45 +0100

    add controller Booking

---
+++ src/Controller/BookingController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BookingController extends AbstractController
+{
+    #[Route('/reservation', name: 'app_booking')]
+    public function index(): Response
+    {
+        return $this->render('booking/index.html.twig');
+    }
+}
