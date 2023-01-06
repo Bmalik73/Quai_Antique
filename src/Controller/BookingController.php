4bc8a842acc1605d7a24de32d145b55b9c46b486
Bmalik73 <bmalik01@live.fr>
2023-01-06 12:11:45 +0100

    add crudBooking

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -2,15 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\Booking;
+use App\Form\BookingType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BookingController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
+
+    }
+
     #[Route('/reservation', name: 'app_booking')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('booking/index.html.twig');
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+
+
+        $booking = new Booking();
+        $form = $this->createForm(BookingType::class, $booking);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $booking = $form->getData();
+            $this->entityManager->persist($booking);
+            $this->entityManager->flush();
+            $this->addFlash('success', 'Votre réservation a bien été envoyé');
+            return $response = $this->render('reservation/modif/index.html.twig', [
+                    'booking' => $booking,
+                ]);
+
+        }
+
+        return $this->render('booking/index.html.twig', [
+            'form' => $form
+        ]);
+
     }
 }
--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\Booking;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +17,44 @@ class BookingType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('hour')
-            ->add('day')
-            ->add('allergy')
-            ->add('seats')
-            ->add('book')
+            ->add('username', TextType::class, [
+                'label'=>'Mail',
+                /*'disabled'=>true,*/
+
+            ])
+            /*->add('name', EntityType::class, [
+                'class' => User::class,
+                'query_builder' => function (EntityRepository $user) {
+                    return $user->createQueryBuilder('u')
+                        ->orderBy('u.lastname', 'ASC');
+                },
+                'choice_label' => 'lastname',
+            ])*/
+            ->add('day', DateType::class, [
+                'label'=>'Jour',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'model_timezone' => 'Europe/Paris',
+            ])
+            ->add('hour', TimeType::class, [
+                'label'=>'heure',
+                'input' => 'datetime',
+                'widget' => 'choice',
+                'hours' => [11, 12, 13, 18, 19, 20, 21],
+                'minutes' => [00, 15, 30, 45],
+            ])
+
+            ->add('allergy', TextType::class, [
+                'label' => 'Allergies',
+                'attr' => [
+                    'placeholder' => 'Veuillez indiquer si vous avez des allergies ou des intolérances alimentaires.'
+                ]
+            ])
+            ->add('seats', IntegerType::class, [
+                'label' => 'Nombre de personnes',
+                'attr' => ['min' => 1, 'max' => 10]
+            ])
+            ->add('submit', SubmitType::class, ['label' => 'Réserver'])
         ;
     }
 
