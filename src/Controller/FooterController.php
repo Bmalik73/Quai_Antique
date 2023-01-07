20320a1cac2c65643524cfc79824a529b271043b
Bmalik73 <bmalik01@live.fr>
2023-01-08 00:47:50 +0100

    add footer

--- src/Controller/FooterController.php
+++ src/Controller/FooterController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Restaurant;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FooterController extends AbstractController
 {
+
     #[Route('/footer', name: 'app_footer')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        // Récupération du restaurant à afficher
+        $restaurant = $entityManager
+            ->getRepository(Restaurant::class)
+            ->find(1);
         return $this->render('footer/index.html.twig', [
-            'controller_name' => 'FooterController',
+            'timeOpenNoon' => $restaurant->getTimeOpenNoon(),
+            'timeCloseNoon' => $restaurant->getTimeCloseNoon(),
+            'timeOpenEvening' => $restaurant->getTimeOpenEvening(),
+            'timeCloseEvening' => $restaurant->getTimeCloseEvening(),
         ]);
     }
+
 }
