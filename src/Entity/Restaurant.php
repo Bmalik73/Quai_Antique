a8bbde01841ab00348b21ffe61075b2fde705151
Bmalik73 <bmalik01@live.fr>
2023-01-07 23:10:41 +0100

    create restaurant entity

---
+++ src/Entity/Restaurant.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RestaurantRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RestaurantRepository::class)]
+class Restaurant
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $timeOpenNoon = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $timeCloseNoon = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $timeOpenEvening = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $timeCloseEvening = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTimeOpenNoon(): ?\DateTimeInterface
+    {
+        return $this->timeOpenNoon;
+    }
+
+    public function setTimeOpenNoon(?\DateTimeInterface $timeOpenNoon): self
+    {
+        $this->timeOpenNoon = $timeOpenNoon;
+
+        return $this;
+    }
+
+    public function getTimeCloseNoon(): ?\DateTimeInterface
+    {
+        return $this->timeCloseNoon;
+    }
+
+    public function setTimeCloseNoon(?\DateTimeInterface $timeCloseNoon): self
+    {
+        $this->timeCloseNoon = $timeCloseNoon;
+
+        return $this;
+    }
+
+    public function getTimeOpenEvening(): ?\DateTimeInterface
+    {
+        return $this->timeOpenEvening;
+    }
+
+    public function setTimeOpenEvening(?\DateTimeInterface $timeOpenEvening): self
+    {
+        $this->timeOpenEvening = $timeOpenEvening;
+
+        return $this;
+    }
+
+    public function getTimeCloseEvening(): ?\DateTimeInterface
+    {
+        return $this->timeCloseEvening;
+    }
+
+    public function setTimeCloseEvening(?\DateTimeInterface $timeCloseEvening): self
+    {
+        $this->timeCloseEvening = $timeCloseEvening;
+
+        return $this;
+    }
+}
