e6c8a1590a0b9d4d82795cf5c436878ff879b28c
Bmalik73 <bmalik01@live.fr>
2023-01-09 22:18:09 +0100

    add is best menu

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Menu;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        $menus = $this->entityManager->getRepository(Menu::class)->findByisBest(1);
+
+        return $this->render('home/index.html.twig',[
+            'menus' => $menus
+        ]);
     }
 }
